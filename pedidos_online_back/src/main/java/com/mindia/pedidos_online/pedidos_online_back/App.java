5698189cface8ca6924076769609c5c007991a80
Daiko <daiko_011@hotmail.com>
2020-12-11 12:31:53 -0300

    Initial back and front commit
    created both projects on eclipse and AS respectively

---
+++ pedidos_online_back/src/main/java/com/mindia/pedidos_online/pedidos_online_back/App.java
@@ -0,0 +1,13 @@
+package com.mindia.pedidos_online.pedidos_online_back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pedidos_online_front/android/app/src/main/kotlin/com/example/pedidos_online_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pedidos_online_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
