56b379e250f69c93d2362df0d134a8bad45270fe
Daiko <daiko_011@hotmail.com>
2020-12-11 12:49:53 -0300

    SpringBoot into back

--- pedidos_online_back/src/main/java/com/mindia/pedidos_online/pedidos_online_back/App.java
+++ pedidos_online_back/src/main/java/com/mindia/pedidos_online/pedidos_online_back/App.java
@@ -1,13 +1,18 @@
 package com.mindia.pedidos_online.pedidos_online_back;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!, this is a Mindia application!" );
+        SpringApplication.run(App.class, args);
     }
 }
