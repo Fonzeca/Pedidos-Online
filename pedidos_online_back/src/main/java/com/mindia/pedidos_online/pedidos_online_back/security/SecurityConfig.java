9bf6a2e47eaffbd21c5a5af34d642c3277e615c1
Daiko <daiko_011@hotmail.com>
2020-12-16 10:17:42 -0300

    sin seguridad, base

--- pedidos_online_back/src/main/java/com/mindia/pedidos_online/pedidos_online_back/security/SecurityConfig.java
+++ pedidos_online_back/src/main/java/com/mindia/pedidos_online/pedidos_online_back/security/SecurityConfig.java
@@ -16,9 +16,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-		.authorizeRequests()
-		.anyRequest().authenticated();
+		http.authorizeRequests().antMatchers("/").
+		permitAll();
 	}
 
 }
