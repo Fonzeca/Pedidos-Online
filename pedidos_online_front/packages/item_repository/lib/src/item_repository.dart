f34f3a2122e9d5d3de332df3901fa0d2faad5be3
Daiko <daiko_011@hotmail.com>
2020-12-18 12:11:51 -0300

    Initialized api calls on item_repository

--- pedidos_online_front/packages/item_repository/lib/src/item_repository.dart
+++ pedidos_online_front/packages/item_repository/lib/src/item_repository.dart
@@ -1,8 +1,54 @@
 
+import 'dart:convert';
+
+import '../item_repository.dart';
+import 'package:http/http.dart' as http;
+
+
 class ServidorRest{
 
+  final client = MindiaHttpClient(http.Client());
   final String IpServer = "http://vps-1791261-x.dattaweb.com";
   final String Port = "4596";
 
-  
+  Future<List<Item>> getAllItems() async{
+
+    String endpoint = "/item/list";
+
+    var response = await client.get(IpServer + ":" + Port + endpoint);
+
+    print("getAllItems/ Status: " + response.statusCode.toString() + " Body: " + response.body);
+
+    if(response.statusCode != 200) throw new Exception ("No se pudo conectar.");
+
+    var jsonData = json.decode(response.body);
+    List<Item> items = [];
+    for(var i in jsonData){
+      Item item = Item.fromJson(i);
+      items.add(item);
+    }
+    return items;
+  }
+
+  Future<List<Item>> getItemsBySection(String section) async{
+    String endpoint = "items/listBySection";
+    String params = "?section"+section;
+
+    var response = await client.get(IpServer + ":" + Port + endpoint + params);
+
+    print("getItemsBySection/ Status: " + response.statusCode.toString() + " Body: " + response.body);
+
+    if(response.statusCode != 200){
+      throw new Exception("No se pudo conectar.");
+    }
+
+    var n = json.decode(response.body);
+    List<Item> items = [];
+    for(var i in jsonData){
+      Item item = Item.fromJson(i);
+      items.add(item);
+    }
+    return items;
+  }
+
 }
--- pedidos_online_front/packages/item_repository/lib/src/models/item.dart
+++ pedidos_online_front/packages/item_repository/lib/src/models/item.dart
@@ -1,1 +1,17 @@
 //TODO: modelar tanto items como secciones.
+class Item{
+  final String name;
+  final String description;
+  final String price;
+  final String image;
+  final String section;
+
+  Item(this.name,this.description,this.price,this.image,this.section);
+
+  Item.fromJson(Map<String, dynamic> json):
+    name = json['name'],
+    description = json['description'],
+    price = json['price'],
+    image = json['image'],
+    section = json['section'];
+}
