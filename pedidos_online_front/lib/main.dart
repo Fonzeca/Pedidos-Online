b61c1ad3c443f678ddd72c0131c7d7ba6e247960
Fragore <alexisfonzos_@hotmail.com>
2020-12-20 20:54:15 -0300

    Improving item label

--- pedidos_online_front/lib/main.dart
+++ pedidos_online_front/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/Alexis%20Fonzo/Desktop/Mindia/Software/PedidosOnline/Pedidos%20Online/pedidos_online_front/lib/src/carta/view/pagina_basico.dart';
+import 'package:pedidos_online_front/src/carta/view/pagina_basico.dart';
 
 void main() => runApp(MyApp());
 
---
+++ pedidos_online_front/lib/src/carta/view/item_label.dart
@@ -0,0 +1,58 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ItemLabel extends StatelessWidget{
+  static String dots;
+
+  final String name;
+  final String price;
+  final String description;
+
+  const ItemLabel({Key key, this.name, this.price, this.description}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 60,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(name),
+              Expanded(
+                child: Text(
+                  getDots(),
+                  maxLines: 1,
+                )
+              ),
+              Text("\$" + price)
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 7),
+            child: Opacity(
+              opacity: 0.5,
+              child: Text(description)
+            )
+          )
+        ],
+      )
+    );
+  }
+
+  static String getDots(){
+    if(dots == null || dots.isEmpty){
+      dots = "";
+      for (var i = 0; i < 10000; i++) {
+        dots+=". ";
+      }
+    }
+    return dots;
+  }
+}
