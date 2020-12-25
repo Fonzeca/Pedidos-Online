6b7329917cacb88412fbcd834a3b1958942d0a0e
Fragore <alexisfonzos_@hotmail.com>
2020-12-25 16:12:39 -0300

    Litte changes

--- pedidos_online_front/lib/src/carta/view/pagina_basico.dart
+++ pedidos_online_front/lib/src/carta/view/pagina_basico.dart
@@ -8,42 +8,33 @@ class basico extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomPadding: false,
-        backgroundColor: Color(0xFFe8e8e8),
-        appBar: AppBar(
-          title: Text('Pedidos Online'),
-          backgroundColor: Color(0xFF30475E),
-        ),
-        body: Center(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.center,
-                    width: 900,
-                    height: 1000,
-                    color: Colors.grey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TituloSection(
-                          titulo: "Bebidas",
-                          descripcion: "Bien frescas",
-                        ),
-                        Expanded(child: _crearGridView()),
-                        TituloSection(
-                          titulo: "Bebidas",
-                          descripcion: "Bien frescas",
-                        ),
-                        Expanded(child: _crearGridView()),
-                      ],
-                    ))
-              ],
+      backgroundColor: Color(0xFFe8e8e8),
+      appBar: AppBar(
+        title: Text('Pedidos Online'),
+        backgroundColor: Color(0xFF30475E),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TituloSection(
+              titulo: "Bebidas",
+              descripcion: "Bien frescas",
             ),
-          ),
-        ));
+            Expanded(child: _crearGridView()),
+            TituloSection(
+              titulo: "Bebidas",
+              descripcion: "Bien frescas",
+            ),
+            Expanded(child: _crearGridView()),
+          ],
+        ),
+      ),
+    );
+
+
   }
 }
 
@@ -52,8 +43,10 @@ class basico extends StatelessWidget {
 Widget _crearGridView() {
   return Container(
       width: 800,
+      height: double.infinity,
       color: Colors.black26,
       child: GridView.count(
+        physics: NeverScrollableScrollPhysics(),
           mainAxisSpacing: 1,
           padding: EdgeInsets.all(8),
           scrollDirection: Axis.vertical,
@@ -87,6 +80,26 @@ Widget _crearGridView() {
               description: "suave y espumoso",
               name: "Vino Fino",
               price: "45",
+            ),
+            ItemLabel(
+              description: "Rico en proteinas",
+              name: "batido con yogurt",
+              price: "10",
+            ),
+            ItemLabel(
+              description: "Rico en proteinas",
+              name: "batido con yogurt",
+              price: "10",
+            ),
+            ItemLabel(
+              description: "Rico en proteinas",
+              name: "batido con yogurt",
+              price: "10",
+            ),
+            ItemLabel(
+              description: "Rico en proteinas",
+              name: "batido con yogurt",
+              price: "10",
             ),
           ])
       //
