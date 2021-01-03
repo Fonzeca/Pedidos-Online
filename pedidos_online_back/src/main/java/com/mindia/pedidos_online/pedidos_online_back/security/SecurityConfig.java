c2806ab8fb7e4c0e7979f1c86ffe2e21f7d82ad7
Fragore <alexisfonzos_@hotmail.com>
2021-01-03 01:57:22 -0300

    Add Cors Origin

--- pedidos_online_back/src/main/java/com/mindia/pedidos_online/pedidos_online_back/security/SecurityConfig.java
+++ pedidos_online_back/src/main/java/com/mindia/pedidos_online/pedidos_online_back/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.mindia.pedidos_online.pedidos_online_back.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
@@ -13,6 +17,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/").
 		permitAll();
+	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("*");
+			}
+		};
+	}
+	
+	@Bean
+	CharacterEncodingFilter characterEncodingFilter() {
+	    CharacterEncodingFilter filter = new CharacterEncodingFilter();
+	    filter.setEncoding("UTF-8");
+	    filter.setForceEncoding(true);
+	    return filter;
 	}
 
 }
